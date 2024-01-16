4b3f513624c93f48a6981b33a78b7ffcb682e19c
Astreex <qwerty94585@mail.ru>
2024-01-16 21:17:01 +0700

    finally

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let firstNumber = '0'
 let secondNumber = '0'
 let sign = ''
 
+const multi = document.querySelector('.multiply').innerHTML
+const division = document.querySelector('.division').innerHTML
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', '×', '÷'];
+const action = ['-', '+', multi, division];
 
 const out = document.querySelector('.output p')
 
@@ -67,6 +69,123 @@ function back() {
     }
 }
 
+document.addEventListener('keypress', (event) => {
+    let key = event.key
+    if (key === '*') {
+        key = multi
+    }
+    if (key === '/') {
+        key = division
+    }
+    if (digit.includes(key)) {
+        if (secondNumber === '0' && sign === '') {
+            if (key === '0' && toString(firstNumber).startsWith('0') && !firstNumber.includes('.')) {
+                firstNumber = '0'
+                out.textContent = firstNumber
+            } else {
+                if (key === '.' && firstNumber.length < 1) {
+                    firstNumber = '0.'
+                    out.textContent = firstNumber
+                } else if (key === '.' && firstNumber.length >= 2 && firstNumber.includes('.')) {
+                    out.textContent = firstNumber
+                } else if (key === '.' && firstNumber.length >= 2 && !firstNumber.includes('.')) {
+                    firstNumber = `${firstNumber}.`
+                    out.textContent = firstNumber
+                } else if (toString(firstNumber).startsWith('0') && firstNumber.length > 0 && !firstNumber.includes('.')) {
+                    firstNumber = firstNumber.substring(1, firstNumber.length)
+                    out.textContent = firstNumber
+                } else {
+                    firstNumber += key
+                    out.textContent = firstNumber
+                }
+            }
+        }
+        else {
+            if (firstNumber != '' && sign == '' && secondNumber != '') {
+                secondNumber = '0'
+            }
+            if (key === '0' && toString(secondNumber).startsWith('0') && !secondNumber.includes('.')) {
+                secondNumber = '0'
+                out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+            } else {
+                if (key === '.' && secondNumber.length < 1) {
+                    secondNumber = '0.'
+                    out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+                } else if (key === '.' && secondNumber.length >= 2 && secondNumber.includes('.')) {
+                    out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+                } else if (key === '.' && secondNumber.length >= 2 && !secondNumber.includes('.')) {
+                    secondNumber = `${secondNumber}.`
+                    out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+                } else if (toString(secondNumber).startsWith('0') && secondNumber.length > 0 && !secondNumber.includes('.')) {
+                    secondNumber = secondNumber.substring(1, secondNumber.length)
+                    out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+                } else {
+                    secondNumber += key
+                    out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+                }
+            }
+        }
+        if (firstNumber.length > 1 && firstNumber[0] === '0' && firstNumber[1] !== '.') {
+            firstNumber = firstNumber.substring(1, firstNumber.length)
+            out.textContent = firstNumber
+        } else if (secondNumber.length > 1 && secondNumber[0] === '0' && secondNumber[1] !== '.') {
+            secondNumber = secondNumber.substring(1, secondNumber.length)
+            out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+        }
+        if (firstNumber == '0' && key == '0' && sign == '') {
+            firstNumber = '0'
+            out.textContent = firstNumber
+        }
+    }
+
+    if (action.includes(key)) {
+        if (firstNumber[firstNumber.length - 1] === '.') {
+            firstNumber = firstNumber.substring(0, firstNumber.length - 1)
+        }
+        if (firstNumber === '') {
+            firstNumber = '0'
+            out.textContent = firstNumber
+        }
+        sign = key
+        out.textContent = `${firstNumber} ${sign} ${secondNumber}`
+        return
+    }
+
+    if (key === '=' || key === 'Enter') {
+        if (secondNumber === '') {
+            secondNumber = firstNumber
+        }
+        switch (sign) {
+            case '+':
+                firstNumber = (+firstNumber) + (+secondNumber)
+                break
+            case '-':
+                firstNumber -= secondNumber
+                break
+            case `${multi}`:
+                firstNumber *= secondNumber
+                break
+            case `${division}`:
+                if (secondNumber === '0') {
+                    out.textContent = `it's impossible bro`;
+                    firstNumber = '0'
+                    secondNumber = ''
+                    sign = ''
+                    return
+                }
+                firstNumber /= secondNumber
+                break
+        }
+        secondNumber = '0'
+        sign = ''
+        out.textContent = firstNumber
+    }
+
+    if (key === 'Backspace') {
+        back()
+    }
+})
+
 document.querySelector('.back').onclick = () => back()
 document.querySelector('.reverse').onclick = () => rev()
 document.querySelector('.clear').onclick = () => clear()
@@ -105,16 +224,9 @@ document.querySelector('.calculator').onclick = (event) => {
                     out.textContent = firstNumber
                 }
             }
-            // }
-            // else if (firstNumber !== '' && secondNumber !== '' && finish) {
-            //     console.log(512231)
-            //     secondNumber = key
-            //     out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            //     finish = false
         }
         else {
             if (firstNumber != '' && sign == '' && secondNumber != '') {
-                console.log(12)
                 secondNumber = '0'
             }
             if (key === '0' && toString(secondNumber).startsWith('0') && !secondNumber.includes('.')) {
@@ -134,43 +246,9 @@ document.querySelector('.calculator').onclick = (event) => {
                     out.textContent = `${firstNumber} ${sign} ${secondNumber}`
                 } else {
                     secondNumber += key
-                    console.log(6234234)
                     out.textContent = `${firstNumber} ${sign} ${secondNumber}`
                 }
             }
-            // if (key === '0' && secondNumber.startsWith('0') && !secondNumber.includes('.')) {
-            //     firstNumber = '0'
-            //     out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            // } else {
-            //     if (key === '.' && secondNumber.length > 1 && secondNumber.includes('.')) {
-            //         out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            //     } else if (key === '.' && secondNumber.length > 1 && !secondNumber.includes('.')) {
-            //         secondNumber = `${secondNumber}.`
-            //         out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            //     } else if (key === '.' && toString(secondNumber).startsWith('0') && secondNumber.length > 0 && !secondNumber.includes('.')) {
-            //         secondNumber = secondNumber.substring(1, secondNumber.length)
-            //         out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            //     } else if (sign == '' && firstNumber != '' && secondNumber) {
-            //         if (key === '.' && secondNumber == '') {
-            //             firstNumber = `${firstNumber}.`
-            //             secondNumber = ''
-            //         }
-            //         console.log(15123)
-            //         firstNumber += key
-            //         out.textContent = `${firstNumber} ${sign}`
-            //     } else {
-            //         if (key === '.' && secondNumber == '') {
-            //             firstNumber = `${firstNumber}.`
-            //             secondNumber = ''
-            //             out.textContent = `${firstNumber}`
-            //             // tut
-            //         }
-            //         if (key === '.' && secondNumber != '') {
-            //             out.textContent = `${firstNumber} ${sign} ${secondNumber}.`                    }
-            //         console.log(15123)
-            //         out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-            //     }
-            // }
         }
         if (firstNumber.length > 1 && firstNumber[0] === '0' && firstNumber[1] !== '.') {
             firstNumber = firstNumber.substring(1, firstNumber.length)
@@ -196,7 +274,6 @@ document.querySelector('.calculator').onclick = (event) => {
         }
         sign = key
         out.textContent = `${firstNumber} ${sign} ${secondNumber}`
-        console.table(firstNumber, secondNumber, sign)
         return
     }
 
@@ -211,7 +288,7 @@ document.querySelector('.calculator').onclick = (event) => {
             case '-':
                 firstNumber -= secondNumber
                 break
-            case '×':
+            case `${multi}`:
                 firstNumber *= secondNumber
                 break
             case '÷':
@@ -228,6 +305,5 @@ document.querySelector('.calculator').onclick = (event) => {
         secondNumber = '0'
         sign = ''
         out.textContent = firstNumber
-        console.table(firstNumber, secondNumber, sign)
     }
 }
